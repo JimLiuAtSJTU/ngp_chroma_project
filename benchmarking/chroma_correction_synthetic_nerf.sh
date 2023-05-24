60db69ad36d9c6636f2a9324cdf279b340188844
zhenhuanliu <954755410@qq.com>
2023-05-25 00:32:05 +0800

    prototype complete.
    more advanced benchmarking.
    
    set opt.py to get chroma std from bash.
    
    proposed method is better than no method.

--- benchmarking/chroma_correction_synthetic_nerf.sh
+++ benchmarking/chroma_correction_synthetic_nerf.sh
@@ -3,24 +3,24 @@
 ROOT_DIR="/home/zhenhuanliu/Desktop/CVproj_ngp_chroma/ngp_chroma_project/data/nerf_synthetic"
 
 
+dash="__"
 
 for std in 0.05 0.1 0.2 0.3 0.4
 do
 for data_name in "lego"  "drums" "ficus" "hotdog" "chair" "materials" "mic" "ship"
 do
-python  train.py --adjust_view_appearance 1 \
-    --root_dir $ROOT_DIR/$data_name \
-    --exp_name $data_name  \
-    --num_epochs 50 --batch_size 16384 --lr 1e-2 --eval_lpips \
-    --chroma_std $std \
-     2>&1 | tee -a $ROOT_DIR/$data_name.log
 
-python  train.py --adjust_view_appearance 0 \
+
+for adjust_ in 1 0
+do
+python  train.py --adjust_view_appearance $adjust_ \
     --root_dir $ROOT_DIR/$data_name \
     --exp_name $data_name  \
     --num_epochs 50 --batch_size 16384 --lr 1e-2 --eval_lpips \
     --chroma_std $std \
-     2>&1 | tee -a $ROOT_DIR/$data_name.log
+     2>&1 | tee -a $ROOT_DIR/$data_name$std$dash$adjust_.log
+done
+
 
 done
 done
